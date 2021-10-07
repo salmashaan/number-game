b8ec443ce9c769aeff5988a87d0f5ed9fea8aac3
salmashaan <smalmashaan@gmail.com>
2021-10-07 11:31:40 +0300

    css

--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -5,35 +5,55 @@ function Bar({ randomNumber }) {
   const [guess, setGuess] = useState(0);
   const [result, setResult] = useState("");
   let results = "";
+  let guesses = [];
+  let previousGuess = 0;
+
+  const updatePreviousGuess = function () {
+    previousGuess = guesses[guesses.length - 1];
+  };
+
+  //   const resetInput = () => {};
 
   const guessButton = () => {
     if (+guess === randomNumber) {
-      setResult(<p className="success">Correct!</p>);
+      setResult(
+        <p className="correct fade-in">Landed on target, well done!!</p>
+      );
     } else if (guess > randomNumber) {
-      setResult(<p className="high">Too high, guess again</p>);
+      setResult(<p className="comment fade-in">Ow, you bumped your head!</p>);
     } else if (guess < randomNumber && guess !== "") {
-      setResult(<p className="low">Too low, guess again</p>);
+      setResult(
+        <p className="comment fade-in">Not even close, jump a bit higher..</p>
+      );
     } else {
       setResult("");
     }
     console.log(results);
   };
 
+  function playAgain() {
+    document.location.reload();
+  }
+
   return (
     <div>
-      {randomNumber}
       <input
         type="number"
         onChange={(e) => {
           setGuess(e.target.value);
         }}
       />
-      <button onClick={guessButton}>GUESS</button>
+      <br /> <br />
+      <button onClick={guessButton}>THIS HIGH</button>
+      <br />
+      <br />
       {result}
+      <br />
+      <button onClick={playAgain}>RESTART</button>
       {console.log(guess)}
     </div>
   );
 }
 
-// <p> {randomNumber} </p> //
+// <p> {randomNumber} </p> // {randomNumber}
 export default Bar;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,14 +6,22 @@ function Head() {
     <div>
       <br />
       <img class="game-logo" src={logo} alt="logo" />
-      <h1> High or Low</h1>
-      <h4>Ready to play?</h4>
+      <h1> When I say jump:</h1>
+      <h1 className="h1-2">you say how high?</h1>
+      <div className="rule">
+        {" "}
+        <p>
+          {" "}
+          <br /> How to play? Let me explain...{" "}
+        </p>{" "}
+      </div>
+      <br />
+      <br />
       <p>
-        <br /> How to play? Let me explain...
-        <br />
-        <br /> All you have you do it guess a number between 1 and 10. And let
-        us do the rest. <br /> <br />
-        That's it! Come on, START!
+        {" "}
+        All you have you do it guess the correct height, between 0 and 10, to
+        reach the target. <br /> <br />
+        That's it! Come on, start JUMPING!
       </p>
       <br />
     </div>
